3516cf60d9c2ad772288df55ebe5ba0709623104
jginesta <jginesta@hawk.iit.edu>
2015-09-30 18:41:05 -0500

    Modified the environment eliminateing spaces

--- install-env.sh
+++ install-env.sh
@@ -4,39 +4,10 @@ sudo apt-get update -y
 
 sudo apt-get install -y apache2 git
 
-
-
-
-
-
-
-
 git clone https://github.com/jginesta/itmo544-444-fall2015.git
 
-
-
-
-
-
-
-
-
-
-
 mv ./itmo-544-444-fall2015/images /var/www/html/images
+
 mv ./itmo-544-444-fall2015/index.html /var/www/html
-A
-B
-A
-A
-B
-A
-mv ./itmo-544-444
-C
-mv /itmo-544-fall2015/index.html /var/www/html
-D
-D
-C
-D
-mv /itmo-544-fall2015/images /var/www/html
+
 echo "Hello!" > /tmp/hello.txt
