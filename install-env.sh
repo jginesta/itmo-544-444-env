29f03bcc8ce9a7584699da81899e04cfae714004
jginesta <jginesta@hawk.iit.edu>
2015-10-02 11:37:09 -0500

    new paths to three repositories

--- install-env.sh
+++ install-env.sh
@@ -5,9 +5,11 @@ sudo apt-get update -y
 sudo apt-get install -y apache2 git
 
 git clone https://github.com/jginesta/itmo544-444-fall2015.git
+git clone https://github.com/jginesta/itmo-544-444-env.git
+git clone https://github.com/jginesta/itmo-544-444-app
 
-mv ./itmo-544-444-fall2015/images /var/www/html/images
+mv ./itmo544-444-fall2015/images /var/www/html/images
 
-mv ./itmo-544-444-fall2015/index.html /var/www/html
+mv ./itmo544-444-fall2015/index.html /var/www/html
 
 echo "Hello!" > /tmp/hello.txt
