944f56f6d8bb46fee4e403cb9f940c07296f8685
jginesta <jginesta@hawk.iit.edu>
2015-10-18 17:50:24 -0500

    installing also php and mysql

--- install-env.sh
+++ install-env.sh
@@ -2,12 +2,15 @@
 
 sudo apt-get update -y
 
-sudo apt-get install -y apache2 git
+
+
+sudo apt-get install -y apache2 git php5 mysql-client
 
 git clone https://github.com/jginesta/itmo544-444-fall2015.git /tmp/web/
 
 sudo mv /tmp/web/* /var/www/html/
 
+sudo mv ./itmo544-444-fall2015/*.php/var/www/html
 sudo service apache2 restart
 
 echo "Hello!" > /tmp/hello.txt
