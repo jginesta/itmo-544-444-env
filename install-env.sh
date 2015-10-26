f3bc4443e82258f56bd4920124e7e3c0d4fe5526
jginesta <jginesta@hawk.iit.edu>
2015-10-25 21:09:02 -0500

    Moving .php files

--- install-env.sh
+++ install-env.sh
@@ -2,15 +2,22 @@
 
 sudo apt-get update -y
 
-
-
-sudo apt-get install -y apache2 git php5 mysql-client
+sudo apt-get install -y apache2 git php5 php5-curl mysql-client curl
 
 git clone https://github.com/jginesta/itmo544-444-fall2015.git /tmp/web/
 
 sudo mv /tmp/web/* /var/www/html/
 
-sudo mv ./itmo544-444-fall2015/*.php/var/www/html
+sudo mv ./itmo544-444-fall2015/*.php /var/www/html
+
+curl -sS https://getcomposer.org/installer | sudo php &> /tmp/getcomposer.txt
+
+sudo php composer.phar require aws/aws-sdk-php &> /tmp/runcomposer.txt
+
+sudo mv vendor /var/www/html &> /tmp/movevendor.txt
+
+sudo php /var/www/html/setup.php &> /tmp/database-setup.txt
+
 sudo service apache2 restart
 
 echo "Hello!" > /tmp/hello.txt
