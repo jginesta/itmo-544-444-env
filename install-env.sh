27711a26da0afed7e7b859ff1b755c71ee43f1dc
jginesta <jginesta@hawk.iit.edu>
2015-10-28 11:19:02 -0500

    added php5-mysql to install

--- install-env.sh
+++ install-env.sh
@@ -2,7 +2,7 @@
 
 sudo apt-get update -y
 
-sudo apt-get install -y apache2 git php5 php5-curl mysql-client curl
+sudo apt-get install -y apache2 git php5 php5-curl mysql-client curl php5-mysql
 
 git clone https://github.com/jginesta/itmo544-444-fall2015.git /tmp/web/
 
