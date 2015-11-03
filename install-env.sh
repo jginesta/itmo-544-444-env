f884ce752400618d55650cf52f67925c36514993
jginesta <jginesta@hawk.iit.edu>
2015-11-02 21:55:57 -0600

    New changes made to a lot of documents

--- install-env.sh
+++ install-env.sh
@@ -2,7 +2,7 @@
 
 sudo apt-get update -y
 
-sudo apt-get install -y apache2 git php5 php5-curl mysql-client curl php5-mysql
+sudo apt-get install -y apache2 git php5 php5-curl mysql-client curl php5-mysql awscli rdscli
 
 git clone https://github.com/jginesta/itmo544-444-fall2015.git /tmp/web/
 
@@ -10,16 +10,19 @@ sudo mv /tmp/web/* /var/www/html/
 
 sudo mv ./itmo544-444-fall2015/*.php /var/www/html
 
-sudo curl -sS https://getcomposer.org/installer | sudo php &> /tmp/getcomposer.txt
+sudo curl -sS https://getcomposer.org/installer | sudo php
+# &> /tmp/getcomposer.txt
 #Falta &>
-sudo php composer.phar require aws/aws-sdk-php &> /tmp/runcomposer.txt
+sudo php composer.phar require aws/aws-sdk-php 
+#&> /tmp/runcomposer.txt
 #Falta &>
-sudo mv vendor /var/www/html &> /tmp/movevendor.txt
+sudo mv vendor /var/www/html 
+#&> /tmp/movevendor.txt
 #Falta &>
 
-aws rds create-db-subnet-group --db-subnet-group-name itmo544-sgn  --subnet-ids subnet-e42819cf subnet-e42819cf --db-subnet-group-description itmosg-jgl
 
-sudo php /var/www/html/setup.php &> /tmp/database-setup.txt
+#sudo php /var/www/html/setup.php
+# &> /tmp/database-setup.txt
 #Falta &>
 sudo service apache2 restart
 
