8b57f7cfbf9d2e6b13c7a0e966d4f280a6c0060d
jginesta <jginesta@hawk.iit.edu>
2015-11-06 10:54:30 -0600

    taking comments away

--- install-env.sh
+++ install-env.sh
@@ -2,7 +2,6 @@
 
 sudo apt-get update -y
 
-#sudo apt-get install -y apache2 git php5 php5-mysql php5-curl mysql-client curl 
 sudo apt-get -y install apache2 git php5 php5-curl mysql-client curl php5-mysql
 
 git clone https://github.com/jginesta/itmo544-444-fall2015.git /tmp/web/
@@ -13,18 +12,13 @@ sudo mv ./itmo544-444-fall2015/*.php /var/www/html
 
 sudo curl -sS https://getcomposer.org/installer | sudo php
 # &> /tmp/getcomposer.txt
-#Falta &>
+
 sudo php composer.phar require aws/aws-sdk-php 
 #&> /tmp/runcomposer.txt
-#Falta &>
+
 sudo mv vendor /var/www/html 
 #&> /tmp/movevendor.txt
-#Falta &>
 
-
-#sudo php /var/www/html/setup.php
-# &> /tmp/database-setup.txt
-#Falta &>
 sudo service apache2 restart
 
 echo "Hello!" > /tmp/hello.txt
