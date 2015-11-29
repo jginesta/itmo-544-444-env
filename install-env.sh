0d83ba10e927ed52a4f1b7ad6f6926bc76ec4ca3
jginesta <jginesta@hawk.iit.edu>
2015-11-29 14:01:13 -0600

    introduced mkdir

--- install-env.sh
+++ install-env.sh
@@ -4,7 +4,9 @@ sudo apt-get update -y
 
 sudo apt-get -y install apache2 git php5 php5-curl mysql-client curl php5-mysql
 
-sudo apt-get install php5-imagick 
+sudo apt-get -y install php5-imagick 
+
+echo "Everything installed correctly!" > /tmp/hello.txt
 
 sudo service apache2 reload
 
@@ -12,10 +14,21 @@ git clone https://github.com/jginesta/itmo544-444-fall2015.git /tmp/web/
 
 sudo mv /tmp/web/* /var/www/html/
 
-sudo mv ./itmo544-444-fall2015/*.php /var/www/html
+sudo mkdir /var/www/html/imagesResult
 
-//sudo mv ./itmo544-444-fall2015/images /var/www/html
+sudo chmod 777 /var/www/html/imagesResult 
 
+sudo chown ubuntu /var/www/html/imagesResult
+
+sudo mkdir /var/www/html/result
+
+sudo chmod 777 /var/www/html/result 
+
+sudo chown ubuntu /var/www/html/result 
+
+sudo chmod 777 /var/www/html/imagick.sh
+
+sudo chown ubuntu /var/www/html/imagick.sh  
 
 sudo curl -sS https://getcomposer.org/installer | sudo php
 # &> /tmp/getcomposer.txt
